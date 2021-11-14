45b4b8c791e7cb8fabe7b880002817a0041a03c7
Akhil-Singampalli <92922964+Akhil-Singampalli@users.noreply.github.com>
2021-11-14 16:02:24 +0530

    Initial commit

---
+++ src/main/java/com/exult/exapp/ExappApplication.java
@@ -0,0 +1,13 @@
+package com.exult.exapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exult/exapp/ExappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exult.exapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
